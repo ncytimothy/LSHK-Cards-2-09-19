2902cbfe49b09e9c7bedfabf9e1719402d30a4c5
Tim Ng <timothyng2008@me.com>
2019-02-09 18:13:34 +0800

    Initial commit

---
+++ LSHK-Cards-2-09-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LSHK-Cards-2-09-19
+//
+//  Created by Tim Ng on 2/9/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
